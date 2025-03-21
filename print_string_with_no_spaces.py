33e42ee4cbb2b1eefc6f7e675ecb3966a43993b9
Kristina Cas <m.kristinacas@gmail.com>
2025-03-21 20:06:08 +0800

    Added Pseudocode for printing a string with no spaces

--- print_string_with_no_spaces.py
+++ print_string_with_no_spaces.py
@@ -1,2 +1,11 @@
 # B5 Prog01: Ask user to input their fullname with spaces at the start
 # Print the input without the spaces in the beginning
+
+"""
+PSEUDOCODE
+
+User input
+print without the spaces
+
+"""
+
