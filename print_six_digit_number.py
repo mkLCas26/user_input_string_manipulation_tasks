26021fb304af87e3b31e6395c281c0f6619f4301
Kristina Cas <m.kristinacas@gmail.com>
2025-03-21 20:52:28 +0800

    Code Written, Add: print six digit numbers, add 0 (final; used a function zfill() to add 0 in the beginning)

--- print_six_digit_number.py
+++ print_six_digit_number.py
@@ -2,6 +2,11 @@
 # Add zeroes at the beginning to complete the 6 digit
 
 """
+PSEUDOCODE
+
 User input
 print input with a function to add zero
 """
+
+user = int(input("Enter number (0-1000): "))
+print(str(user).zfill(6))
