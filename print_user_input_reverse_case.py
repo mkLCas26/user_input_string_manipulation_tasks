c3d3bd6dba9369de76d34e70f21d0e840cc017db
Kristina Cas <m.kristinacas@gmail.com>
2025-03-21 21:28:41 +0800

    Added Pseudocode for printing string in reversed case

--- print_user_input_reverse_case.py
+++ print_user_input_reverse_case.py
@@ -1,2 +1,9 @@
 # B5 Prog06: Ask user to input fullname with incorrect casing
 # Reverse the case of each letter from input
+
+"""
+PSEUDOCODE
+
+ask user input (incorrect casing)
+print in reversed case using function
+"""
