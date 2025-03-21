5c771d6d8ab27da8d7501f40a5245daa9a548efd
Kristina Cas <m.kristinacas@gmail.com>
2025-03-21 22:08:32 +0800

    Added Pseudocode for printing string in PascalCase

--- print_user_input_pascal_case.py
+++ print_user_input_pascal_case.py
@@ -1,2 +1,9 @@
 # B5 Prog09: Ask user to input fullname with incorrect casing
 # Print input in pascal case
+
+"""
+PSEUDOCODE
+
+ask user input
+print in pascal case using functions: split() and title()
+"""
