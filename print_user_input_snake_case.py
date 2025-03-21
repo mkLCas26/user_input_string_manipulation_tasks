b56a0b471269d21444c3ef89eebe1938362c3c5a
Kristina Cas <m.kristinacas@gmail.com>
2025-03-21 22:18:17 +0800

    Added Pseudocode for printing string in snake_case

--- print_user_input_snake_case.py
+++ print_user_input_snake_case.py
@@ -1,2 +1,9 @@
 # B5 Prog10: Ask user to input fullname with incorrect casing
 # Print in snake_case
+
+"""
+PSEUDOCODE
+
+ask user input
+print in snake_case using the functions: replace() and lower()
+"""
